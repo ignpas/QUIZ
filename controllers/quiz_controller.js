10175f0bf564751867adf471458c4cb31b8de307
Ignacio Pascual <ignpas@hotmail.com>
2015-05-08 16:28:07 +0200

    Despliegue Heroku Portugal

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,35 @@
 var models = require('../models/models.js');
 
+//Autoload -- factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+			req.quiz=quiz;
+			next();
+
+		}else{ next(new Error('No existe quizId=' + quizId));}
+	}
+		).catch(function(error) {next(error);});
+};
+
 //GET /quizes
 exports.index=function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/indez.ejs',{ quizes: quizes});
+		res.render('quizes/index',{ quizes: quizes});
 
-	})
+	}).catch(function(error){next(error);})
 };
 
-
-
-
-
-
-// GET /quizes/question
+// GET /quizes/:id
 
 exports.show = function (req,res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-	res.render('quizes/show' , {pregunta: quiz});	
-})
+//	models.Quiz.find(req.params.quizId).then(function(quiz){
+	res.render('quizes/show' , {quiz: req.quiz});	
+//})
 };
 
-// GET /quizes/answer
+// GET /quizes/:id/answer
 
 exports.answer= function (req,res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz){
@@ -31,4 +39,11 @@ exports.answer= function (req,res) {
 		res.render('quizes/answer' , {respuesta: 'Incorrecto'});
 	}
 })
+};
+exports.answer = function(req, res){
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta===req.quiz.respuesta){
+		resultado= 'Correcto';
+	}
+	res.render('quizes/answer',{quiz: req.quiz, respuesta:resultado});
 };
