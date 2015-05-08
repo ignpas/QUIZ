1be0e89d73576ac5cf50a44200310fc63b7bc5e5
Ignacio Pascual <ignpas@hotmail.com>
2015-05-08 15:50:36 +0200

    Heroku pregunta Portugal

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,31 @@
 var models = require('../models/models.js');
+
+//GET /quizes
+exports.index=function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/indez.ejs',{ quizes: quizes});
+
+	})
+};
+
+
+
+
+
+
 // GET /quizes/question
 
-exports.question = function (req,res) {
-	models.Quiz.findAll().success(function(quiz){
-	res.render('quizes/question' , {pregunta: quiz[0].pregunta});	
+exports.show = function (req,res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+	res.render('quizes/show' , {pregunta: quiz});	
 })
 };
 
 // GET /quizes/answer
 
 exports.answer= function (req,res) {
-	models.Quiz.findAll().success(function(quiz){
-	if(req.query.respuesta === quiz[0].respuesta){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+	if(req.query.respuesta === quiz.respuesta){
 		res.render('quizes/answer' , {respuesta: 'Correcto'});
 	} else {
 		res.render('quizes/answer' , {respuesta: 'Incorrecto'});
